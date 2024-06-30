fdaba0cb8f3abe17b10c233a10b6e7449fe24ced
lonely43 <iladenisov327@gmail.com>
2024-06-30 16:50:33 +0300

    fix filters

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,9 +1,8 @@
-let selected = ["vanilla", "sass", "react", "angular", "vue"]
+let selected = []
 let filters = document.querySelectorAll("main .search ul li input")
 let cards = document.querySelectorAll(".projects .card")
 
 filters.forEach(el => {
-   el.checked = true
    el.addEventListener('click', () => {
       if(selected.indexOf(el.value) < 0)
          selected.push(el.value)
@@ -15,19 +14,32 @@ filters.forEach(el => {
    }, false)
 })
 
-function showCards(array){
+function showCards(selected){
    let filteredCards = []
 
-   cards.forEach(el => {
-      array.forEach(arrEl => {
-         if(el.getAttribute("marks").split(" ").indexOf(arrEl) >= 0){
-            filteredCards.push(el)
+   if(selected.length == 0){
+      filteredCards = cards
+   }
+   else{
+      cards.forEach((el, index) => {
+         let marks = el.getAttribute("marks").split(" ")
+
+         if(isLegit(marks, selected)){
+            if(filteredCards.indexOf(el) < 0){
+               filteredCards.push(el)
+            }
+         }
+         else{
+            if(filteredCards.indexOf(el) >= 0){
+               filteredCards.splice(selected.indexOf(index),1)
+            }
          }
       })
-   })
+   }
 
-   document.querySelector(".projects").innerHTML = ``
+   document.querySelector(".projects").innerHTML = `` // clear section
 
+   // in null case
    if(filteredCards.length <= 0){
       document.querySelector(".projects").innerHTML= `<h1 style="font-weight:400;text-align:center;">there's nothing here :(</h1>`
    }
@@ -36,4 +48,16 @@ function showCards(array){
          document.querySelector(".projects").appendChild(el)
       })
    }
+}
+
+function isLegit(marks, selected){
+   let legit = true
+
+   for(i = 0; i < selected.length; i++){
+      if(!(marks.includes(selected[i]))){
+         return false
+      }
+   }
+
+   return legit
 }
